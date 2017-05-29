f5550cb016e41611e183a53864792fa91e3210ea
Anisha John <anishajohn@Anishas-MacBook-Pro.local>
2017-05-29 16:23:05 -0400

    Initial Commit

---
+++ aj first ios app w udemy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aj first ios app w udemy
+//
+//  Created by Anisha John on 5/29/17.
+//  Copyright © 2017 Anisha John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
